80ad40915b5de790a08a7e84b2b5f4c686861ae8
ElysiaVilladarez <elysia@saperium.com>
2019-03-12 20:45:54 +0800

    Added magna carta, learn more and benefits

--- thisability/src/app/home/benefits/benefits.component.ts
+++ thisability/src/app/home/benefits/benefits.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./benefits.component.css']
 })
 export class BenefitsComponent implements OnInit {
-
+  benefitsArray = null;
   constructor() { }
 
   ngOnInit() {
+    this.benefitsArray = [
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'hotels and similar lodging<br>establishments'
+    },
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'restaurants'
+    },
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'recreation centers'
+    },
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'theaters and other similar<br>places of culture, leisure and<br>amusement'
+    },
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'medicine'
+    },
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'medical and dental services'
+    },
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'domestic air and sea travel'
+    },
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'public railways, skyways, and<br>other public utility vehicles'
+    },
+    {
+      imgUrl: '../../../assets/bg-placeholder.png',
+      caption: 'funeral and burial services for<br>the death of the PWD'
+    }];
   }
 
 }
